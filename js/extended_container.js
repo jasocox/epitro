737f51f5e4aa747d2d8c5390e6a41eaafeafc07e
Jason Cox <jasonwilburcox@gmail.com>
2017-03-25 00:43:53 -0400

    Project setup

---
+++ js/extended_container.js
@@ -0,0 +1,14 @@
+(function() {
+
+  function ExtendedContainer() {
+    this.Container_constructor();
+  }
+
+  var p = createjs.extend(ExtendedContainer, createjs.Container);
+  p.output = function() {
+    console.log("Console Output Test.");
+  };
+
+  window.ExtendedContainer = createjs.promote(ExtendedContainer, "Container");
+
+})();
