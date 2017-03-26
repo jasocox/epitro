fa68d1c0ecdaa53f7048754783bab9e117b3b311
Jason Cox <jasonwilburcox@gmail.com>
2017-03-25 23:38:40 -0400

    Adding dots

--- js/main.js
+++ js/main.js
@@ -1,27 +1,19 @@
 function main() {
 
   // Setup
-  setup();
+  var stage = setupStage(document.getElementById("canvas"));
 
   // Keyboard Test
   document.onkeydown = keyPressed;
 
-  // Display Test
-  var testing = new createjs.Shape();
-
-  //testing.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 50);
-  testing.graphics.beginFill("DeepSkyBlue").rect(0, 0, 50, 50);
-  testing.originX = testing.x;
-  testing.counter = 0;
-  testing.increment = 0.1;
-  testing.amplitude = 50;
-  testing.on("tick", update);
-
-  container.addChild(testing);
-
   // Extension Test
   var extend_test = new ExtendedContainer();
   extend_test.output();
+
+  setInterval(function() {
+    stage.addChild(generateDot());
+    stage.update();
+  }, 1);
 
 }
 
@@ -30,11 +22,4 @@ function keyPressed(event) {
   if (event.keyCode === 32) {
     console.log("testing");
   }
-}
-
-function update(event) {
-  event.target.x = event.target.originX +
-    Math.sin(event.target.counter) * event.target.amplitude;
-
-  event.target.counter += event.target.increment;
 }
